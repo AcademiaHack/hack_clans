01a328d8cc37a566fd0a743770bfdc5507c5d374
jrodriguezmoodhack <jrodriguez.moodhack@gmail.com>
2016-08-29 07:53:21 -0400

    Create dwarf.rb
    Dwarf
    - 3 Skills
    - Seteado de forma que mantenga un balance: Bajos cooldowns con suficiente daño, en total: 8 de Cooldown, 55 daño, y 20 de defensa.

---
+++ dwarf.rb
@@ -0,0 +1,21 @@
+require_relative "character"
+require_relative "special_action"
+
+class Dwarf < Character
+
+  def turn
+  end
+
+  def hammer_blow!
+    SpecialAction.new "Hammer Blow!!", 1, {health: 10, defense: 5, push: 1}
+  end
+
+  def pickaxe_throwing!
+    SpecialAction.new "This isn't just for getting gold, you know.", 3, {health:20, defense: 20, push: 0}
+  end
+
+  def bom!
+    SpecialAction.new "BAAAAAAAAANG!!", 4, {health: 25, defense: 15, push: 1} 
+  end
+
+end
